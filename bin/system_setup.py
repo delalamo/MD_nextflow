f01c5cac4cf4a12594eee9722372064c273b65cb
Diego del Alamo <diego.delalamo@gmail.com>
2025-03-18 10:15:44 +0100

    Adding PEP-8 compliant docstrings to system_setup.py

--- bin/system_setup.py
+++ bin/system_setup.py
@@ -13,9 +13,13 @@ from pdbfixer import PDBFixer
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 def parse_args():
     """
-    Parse command line arguments
+    Parse command line arguments.
+
+    Returns:
+        argparse.Namespace: Parsed command line arguments.
     """
     argparser = argparse.ArgumentParser()
     argparser.add_argument("-i", "--input_pdb", required=True, type=str)
@@ -23,11 +27,16 @@ def parse_args():
     argparser.add_argument("-v", "--verbose", action="store_true")
     return argparser.parse_args()
 
+
 def fix_structure(pdbfile_in: str, pdbfile_out: str):
     """
-    Fixed PDB by adding loops and replacing nonstandard AAs with standard 
-    equivalents (for example, selenomethionine to methionine).
-    Does not add hydrogens; this is done later.
+    Fix PDB by adding loops and replacing nonstandard amino acids with standard
+    equivalents (e.g., selenomethionine to methionine). Does not add hydrogens;
+    this is done later.
+
+    Args:
+        pdbfile_in (str): Input PDB file path.
+        pdbfile_out (str): Output PDB file path.
     """
     fixer = PDBFixer(filename=pdbfile_in)
     fixer.findMissingResidues()
@@ -36,26 +45,37 @@ def fix_structure(pdbfile_in: str, pdbfile_out: str):
     fixer.removeHeterogens(False)
     fixer.findMissingAtoms()
     fixer.addMissingAtoms()
-    app.PDBFile.writeFile(
-        fixer.topology,
-        fixer.positions,
-        open(pdbfile_out, 'w'))
+    app.PDBFile.writeFile(fixer.topology, fixer.positions, open(pdbfile_out, "w"))
+
 
 def solvate_protein(
-        pdbfile_in: str,
-        pdbfile_out: str,
-        gmx_prefix: str="SYSTEM",
-        forcefield: str="amber14-all.xml",
-        water_model: str="amber14/tip3pfb.xml",
-        ion_strength: float=0.15,
-        positive_ion: str="K+",
-        negative_ion: str="Cl-",
-        padding: float=1.0,
-        temperature: float=300.0,
-        time_step_fs: float=2.0
-        ):
+    pdbfile_in: str,
+    pdbfile_out: str,
+    gmx_prefix: str = "SYSTEM",
+    forcefield: str = "amber14-all.xml",
+    water_model: str = "amber14/tip3pfb.xml",
+    ion_strength: float = 0.15,
+    positive_ion: str = "K+",
+    negative_ion: str = "Cl-",
+    padding: float = 1.0,
+    temperature: float = 300.0,
+    time_step_fs: float = 2.0,
+):
     """
-    Solvates model and adds ions.
+    Solvate model and add ions.
+
+    Args:
+        pdbfile_in (str): Input PDB file path.
+        pdbfile_out (str): Output PDB file path.
+        gmx_prefix (str): Prefix for GROMACS files.
+        forcefield (str): Forcefield file path.
+        water_model (str): Water model file path.
+        ion_strength (float): Ionic strength in molar.
+        positive_ion (str): Positive ion type.
+        negative_ion (str): Negative ion type.
+        padding (float): Padding distance in nanometers.
+        temperature (float): Simulation temperature in Kelvin.
+        time_step_fs (float): Time step in femtoseconds.
     """
     pdb = app.PDBFile(pdbfile_in)
     forcefield = app.ForceField(forcefield, water_model)
@@ -64,57 +84,56 @@ def solvate_protein(
 
     modeller.addSolvent(
         forcefield,
-        padding=padding*openmm.unit.nanometers,
-        ionicStrength=ion_strength*openmm.unit.molar,
+        padding=padding * openmm.unit.nanometers,
+        ionicStrength=ion_strength * openmm.unit.molar,
         positiveIon=positive_ion,
-        negativeIon=negative_ion)
-    
+        negativeIon=negative_ion,
+    )
+
     system = forcefield.createSystem(
-        modeller.topology, 
+        modeller.topology,
         nonbondedMethod=app.PME,
-        nonbondedCutoff=1*openmm.unit.nanometer,
+        nonbondedCutoff=1 * openmm.unit.nanometer,
         constraints=app.HBonds,
-        rigidWater=False) # required to convert to gromacs
+        rigidWater=False,
+    )  # required to convert to gromacs
 
     integrator = openmm.LangevinMiddleIntegrator(
-        temperature*openmm.unit.kelvin,
-        1/openmm.unit.picosecond,
-        time_step_fs*openmm.unit.femtoseconds
+        temperature * openmm.unit.kelvin,
+        1 / openmm.unit.picosecond,
+        time_step_fs * openmm.unit.femtoseconds,
     )
 
-    simulation = app.Simulation(
-        modeller.topology,
-        system,
-        integrator
-    )
+    simulation = app.Simulation(modeller.topology, system, integrator)
     simulation.context.setPositions(modeller.positions)
     simulation.minimizeEnergy()
     positions = simulation.context.getState(getPositions=True).getPositions()
 
     app.PDBFile.writeFile(
-        simulation.topology,
-        positions,
-        open(os.path.basename(pdbfile_out), 'w'))
+        simulation.topology, positions, open(os.path.basename(pdbfile_out), "w")
+    )
 
     pmd_structure = parmed.openmm.load_topology(
-        simulation.topology,
-        system=system,
-        xyz=positions)
+        simulation.topology, system=system, xyz=positions
+    )
 
     pmd_structure.save(f"{gmx_prefix}.top", overwrite=True)
     pmd_structure.save(f"{gmx_prefix}.gro", overwrite=True)
 
+
 def main():
     """
-    Main FXN
+    Main function to execute the script.
     """
     args = parse_args()
     pdbfile, out_filename = args.input_pdb, args.output_pdb
-    if args.verbose: logging.basicConfig(level=logging.DEBUG)
-        
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
     temp_pdb_filename = f"fixed_{os.path.basename(pdbfile)}"
     fix_structure(pdbfile, temp_pdb_filename)
     simulation = solvate_protein(temp_pdb_filename, out_filename)
+
 
 if __name__ == "__main__":
     main()
